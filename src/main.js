5f5e00e1ddebd19eaec36713aa8e3523aece4999
Terry Amagboro <rukkiecodes@gmail.com>
2021-12-26 14:58:05 +0100

    initialized

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import Vuesax from "vuesax"
+import "vuesax/dist/vuesax.css"
+
+Vue.use(Vuesax)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app")
