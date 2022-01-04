37390adeeeb054540e90ba33c07eb67e7bfe85b9
Terry Amagboro <rukkiecodes@gmail.com>
2022-01-05 00:37:06 +0100

    can now paginate

--- src/components/template/Cataloge.vue
+++ src/components/template/Cataloge.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cataloge">
     <div class="cardRows">
-      <vs-card class="card" v-for="(design, i) in templates" :key="i">
+      <vs-card class="card" v-for="(design, i) in displayedTemplates" :key="i">
         <template #text>
           <img :src="design.image" alt="" />
           <div class="label">
@@ -25,29 +25,86 @@
         </template>
       </vs-card>
     </div>
+
+    <div class="paginationWraper">
+      <vs-pagination progress not-margin v-model="page" :length="pages.length" />
+      <!-- <vs-button :disabled="page <= 1" @click="page--" icon>
+        <i class="las la-angle-double-left"></i>
+      </vs-button>
+      <vs-button
+        v-for="(pageNumber, i) in pages.slice(page - 1, page + 5)"
+        :key="i"
+        @click="page = pageNumber"
+      >
+        {{ pageNumber }}
+      </vs-button>
+      <vs-button
+        :disabled="page >= Math.ceil(templates.length / perPage)"
+        @click="page++"
+        icon
+      >
+        <i class="las la-angle-double-right"></i>
+      </vs-button> -->
+    </div>
     <Preview />
   </div>
 </template>
 
 <script>
+// @ts-nocheck
 import { mapActions, mapGetters, mapState } from "vuex"
 import Preview from "./components/Preview.vue"
-// @ts-nocheck
 export default {
+  data: () => ({
+    page: 1,
+    perPage: 9,
+    pages: [],
+  }),
+
   components: {
     Preview,
   },
+
+  watch: {
+    templates() {
+      this.setPages()
+    },
+  },
+
   mounted() {
     this.$nextTick(() => {
       this.getTamplates()
     })
   },
+
   methods: {
     ...mapActions(["getTamplates", "previewTemplate"]),
+
+    setPages() {
+      let number_of_pages = Math.ceil(
+        this.$store.getters.templates.length / this.perPage
+      )
+      for (let index = 1; index <= number_of_pages; index++) {
+        this.pages.push(index)
+      }
+    },
+
+    paginate(templates) {
+      let page = this.page
+      let perPage = this.perPage
+      let from = page * perPage - perPage
+      let to = page * perPage
+      return templates.slice(from, to)
+    },
   },
+
   computed: {
     ...mapGetters(["templates"]),
     ...mapState(["template"]),
+
+    displayedTemplates() {
+      return this.paginate(this.$store.getters.templates)
+    },
   },
 }
 </script>
